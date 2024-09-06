c26d700a929bac3fe1e1a6896bf928931b197d34
songsongyl <2444616264@qq.com>
2024-09-06 10:41:46 +0800

    spring-jdbc-examples

---
+++ spring-jdbc-examples/src/main/java/image/springjdbcexamples/SpringJdbcExamplesApplication.java
@@ -0,0 +1,13 @@
+package image.springjdbcexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcExamplesApplication.class, args);
+    }
+
+}
---
+++ spring-jdbc-examples/src/test/java/image/springjdbcexamples/SpringJdbcExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package image.springjdbcexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
