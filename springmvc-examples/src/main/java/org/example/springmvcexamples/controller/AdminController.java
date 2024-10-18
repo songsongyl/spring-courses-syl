fba3809e997ef26b3d7351978ed9af114d2f486b
songsongyl <2444616264@qq.com>
2024-10-18 10:02:31 +0800

    springmvc

--- springmvc-examples/src/main/java/org/example/springmvcexamples/controller/AdminController.java
+++ springmvc-examples/src/main/java/org/example/springmvcexamples/controller/AdminController.java
@@ -2,6 +2,8 @@ package org.example.springmvcexamples.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.springmvcexamples.dox.User;
+import org.example.springmvcexamples.exception.Code;
 import org.example.springmvcexamples.service.UserService;
 import org.example.springmvcexamples.vo.ResultVo;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,11 @@ public class AdminController {
 
     @GetMapping("users/{account}")
     public ResultVo getUser(@PathVariable String account){
-        return ResultVo.success(userService.getUserByAccount(account));
+       User user =  userService.getUserByAccount(account);
+       if(user == null){
+           return ResultVo.error(Code.BAD_REQUEST);
+       }
+       return ResultVo.success(user);
+//        return ResultVo.success(userService.getUserByAccount(account)); 感觉设计有点不合理
     }
 }
